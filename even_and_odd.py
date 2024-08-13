10608b129e79819d36a229f6004549353e9eb21f
NdumisoMbili <sn.mbili01@gmail.com>
2024-08-13 21:33:08 +0200

    even_and_odd repository

---
+++ even_and_odd.py
@@ -0,0 +1,8 @@
+# Let the user enter a number
+num = int(input("Enter a number: "))
+
+# Check if the number is even or odd using the if and else statement 
+if num % 2 == 0 :
+    print(f"{num} is an even number")
+else:
+    print("is an odd number")
